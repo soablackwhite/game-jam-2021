6ff8996cc74f44d64e659a5ee8217e7bfc9078f9
Shantanu Bhatia <shantanubhatia@gmail.com>
2021-01-30 09:30:59 +0530

    cleaned up comments, removed useless print

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,32 +5,30 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;                //Floating point variable to store the player's movement speed.
+    public float speed;              
 
-    private Rigidbody2D rb2d;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private Rigidbody2D rb2d;
 
-    // Use this for initialization
+
     void Start()
     {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    // FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        //Store the current horizontal input in the float moveHorizontal.
+        // Basic movement input
+        // TODO - refactor to be able to choose what keys affect the Input axes
         float moveHorizontal = Input.GetAxis("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
         float moveVertical = Input.GetAxis("Vertical");
 
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        Debug.Log(movement);
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        // Set the velocity property of our Rigidbody2D rb2d to give our player some movement.
+        // Elected to use velocity instead of AddForce because AddForce feels really floaty.
         rb2d.velocity = movement * speed;
     }
 }
