1c3ff071a37b0e5d6b84f1734a64e8fff7753964
Hatim <hb1487@nyu.edu>
2021-01-30 22:18:50 +0400

    Auto stash before merge of "main" and "origin/neonovi"

---
+++ Assets/Scripts/pathManager.cs
@@ -0,0 +1,209 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// MAZE PROC GEN LAB
+// all students: complete steps 1-6, as listed in this file
+// optional: if you're up for a bit of a mind safari, complete the "extra tasks" to do at the very bottom
+
+// STEP 1: ======================================================================================
+// put this script on a Sphere... it SHOULD move around, and drop a path of floor tiles behind it
+
+public class pathManager : MonoBehaviour {
+
+// STEP 2: ============================================================================================
+// translate the pseudocode below
+
+//	DECLARE CLASS MEMBER VARIABLES:
+//	Declare a private integer called counter that starts at 0; 		// counter will track how many floor tiles I've instantiated
+//	Declare a public Transform called floorPrefab, assign the prefab in inspector;
+//	Declare a public Transform called pathmakerSpherePrefab, assign the prefab in inspector; 		// you'll have to make a "pathmakerSphere" prefab later
+	private int counter=0;
+	public GameObject floorPrefab;
+	public GameObject pathmakerSpherePrefab;
+	public static int floorNum=0;
+	public static int sphereNumber=1;
+	public static int endedSpheres=0;
+	public int staticFloor=0;
+	public int maxTiles=1000;
+	public float turnProbability=0.25f;
+	public int roomSize=30;
+	public int corridorSize=20;
+	public bool roomMode=true;
+	bool end=false;
+
+	private float originalProb;
+	private int originalCorSize, originalRoomSize;
+
+	Vector3 positions;
+	float maxDistance;
+	public static Vector3 origin=new Vector3(10f,10f,10f);
+	public float movementUnit=0.1f;
+
+	void Start(){
+		positions=Camera.main.gameObject.transform.position;
+		maxDistance=Camera.main.orthographicSize;
+		if(origin==new Vector3(10f,10f,10f)){
+			origin=transform.position;
+			print("new origin");
+		}
+		originalProb=turnProbability;
+		originalCorSize=corridorSize;
+		originalRoomSize=roomSize;
+	}
+
+	void Update () {
+		staticFloor=floorNum;
+//		If counter is less than 50, then:
+//			Generate a random number from 0.0f to 1.0f;
+//			If random number is less than 0.25f, then rotate myself 90 degrees;
+//				... Else if number is 0.25f-0.5f, then rotate myself -90 degrees;
+//				... Else if number is 0.99f-1.0f, then instantiate a pathmakerSpherePrefab clone at my current position;
+//			// end elseIf
+
+		if (floorNum<maxTiles && !end){
+			float f=Random.Range(0f, 1f);
+			
+			if(f<turnProbability){
+				transform.Rotate(0,0,90);
+			}else if(f<turnProbability*2f){
+				transform.Rotate(0,0,-90);
+			}else if(f>0.925f && f<1f){
+				GameObject newSphere;
+				newSphere=Instantiate(pathmakerSpherePrefab,transform.position,transform.rotation,transform.parent);
+				print("new sphere");
+				sphereNumber++;
+			}
+//			Instantiate a floorPrefab clone at current position;
+//			Move forward ("forward", as in, the direction I'm currently facing) by 5 units;
+//			Increment counter;
+//		Else:
+//			Destroy my game object; 		// self destruct if I've made enough tiles already
+			bool floorAdded=false;
+			int tries=0;
+			while(!floorAdded && tries<=4){
+				transform.Translate(Vector3.right*movementUnit);
+				if(Physics2D.OverlapPoint(transform.position)==null){
+					GameObject newFloor;
+					newFloor=Instantiate(floorPrefab,transform.position,new Quaternion(0,0,0,0),transform.parent);
+					positions=positions+transform.position;
+					floorAdded=true;
+				}else{
+					transform.Translate(Vector3.left*movementUnit);
+					transform.Rotate(0,0,90);
+					tries++;
+				}
+			}
+			
+			if(floorAdded){
+				floorNum++;
+				Vector3 center=Vector3.Lerp(Camera.main.gameObject.transform.position,transform.position,1/floorNum);
+				//Camera.main.gameObject.transform.position=new Vector3(positions.x/floorNum,positions.y/floorNum,Camera.main.gameObject.transform.position.z);
+				//Camera.main.gameObject.transform.position=new Vector3(center.x,center.y,Camera.main.gameObject.transform.position.z);
+				float distance=Vector3.Distance(origin,transform.position);
+				maxDistance=Mathf.Max(Camera.main.orthographicSize,distance);
+				//Camera.main.orthographicSize=maxDistance;
+				counter++;
+				if(roomMode && counter>roomSize){
+					turnProbability=0.01f;
+					roomSize=Random.Range(originalRoomSize-10,originalRoomSize+20);
+					counter=0;
+					roomMode=!roomMode;
+				}else if(!roomMode && counter>corridorSize){
+					turnProbability=originalProb;
+					corridorSize=Random.Range(originalCorSize-3,originalCorSize+3);
+					counter=0;
+					roomMode=!roomMode;
+				}
+			}else{
+				end=true;
+				endedSpheres++;
+			}
+		}else{
+			//Destroy(gameObject);
+		}
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			floorNum=0;
+			origin=new Vector3(10f,10f,10f);
+			Scene scene = SceneManager.GetActiveScene(); 
+			SceneManager.LoadScene(scene.name);
+			print("r pressed");
+		}
+	
+	}
+
+
+
+} 
+
+// MORE STEPS BELOW!!!........
+
+// STEP 3: =====================================================================================
+// implement, test, and stabilize the system
+
+//	IMPLEMENT AND TEST:
+//	- save your scene! the code could potentially be infinite / exponential, and crash Unity
+//	- put Pathmaker.cs on a sphere, configure all the prefabs in the Inspector, and test it to make sure it works
+//	STABILIZE: 
+//	- code it so that all the Pathmakers can only spawn a grand total of 500 tiles in the entire world; how would you do that?
+//	- hint: declare a "public static int" and have each Pathmaker check this "globalTileCount", somewhere in your code? 
+//      -  What is a 'static'?  Static???  Simply speak the password "static" to the instructor and knowledge will flow.
+//	- Perhaps... if there already are enough tiles maybe the Pathmaker could Destroy my game object
+
+// STEP 4: ======================================================================================
+// tune your values...
+
+// a. how long should a pathmaker live? etc.  (see: static  ---^)
+// b. how would you tune the probabilities to generate lots of long hallways? does it... work?
+// c. tweak all the probabilities that you want... what % chance is there for a pathmaker to make a pathmaker? is that too high or too low?
+
+
+
+// STEP 5: ===================================================================================
+// maybe randomize it even more?
+
+// - randomize 2 more variables in Pathmaker.cs for each different Pathmaker... you would do this in Start()
+// - maybe randomize each pathmaker's lifetime? maybe randomize the probability it will turn right? etc. if there's any number in your code, you can randomize it if you move it into a variable
+
+
+
+// STEP 6:  =====================================================================================
+// art pass, usability pass
+
+// - move the game camera to a position high in the world, and then point it down, so we can see your world get generated
+// - CHANGE THE DEFAULT UNITY COLORS
+// - add more detail to your original floorTile placeholder -- and let it randomly pick one of 3 different floorTile models, etc. so for example, it could randomly pick a "normal" floor tile, or a cactus, or a rock, or a skull
+// - or... make large city tiles and create a city.  Set the camera low so and une the values so the city tiles get clustered tightly together.
+
+//		- MODEL 3 DIFFERENT TILES IN BLENDER.  CREATE SOMETHING FROM THE DEEP DEPTHS OF YOUR MIND TO PROCEDURALLY GENERATE. 
+//		- THESE TILES CAN BE BASED ON PAST MODELS YOU'VE MADE, OR NEW.  BUT THEY NEED TO BE UNIQUE TO THIS PROJECT AND CLEARLY TILE-ABLE.
+
+//		- then, add a simple in-game restart button; let us press [R] to reload the scene and see a new level generation
+// - with Text UI, name your proc generation system ("AwesomeGen", "RobertGen", etc.) and display Text UI that tells us we can press [R]
+
+
+// OPTIONAL EXTRA TASKS TO DO, IF YOU WANT / DARE: ===================================================
+
+// AVOID SPAWNING A TILE IN THE SAME PLACE AS ANOTHER TILE  https://docs.unity3d.com/ScriptReference/Physics.OverlapSphere.html
+// Check out the Physics.OverlapSphere functionality... 
+//     If the collider is overlapping any others (the tile prefab has one), prevent a new tile from spawning and move forward one space. 
+
+// DYNAMIC CAMERA:
+// position the camera to center itself based on your generated world...
+// 1. keep a list of all your spawned tiles
+// 2. then calculate the average position of all of them (use a for() loop to go through the whole list) 
+// 3. then move your camera to that averaged center and make sure fieldOfView is wide enough?
+
+// BETTER UI:
+// learn how to use UI Sliders (https://unity3d.com/learn/tutorials/topics/user-interface-ui/ui-slider) 
+// let us tweak various parameters and settings of our tech demo
+// let us click a UI Button to reload the scene, so we don't even need the keyboard anymore.  Throw that thing out!
+
+// WALL GENERATION
+// add a "wall pass" to your proc gen after it generates all the floors
+// 1. raycast downwards around each floor tile (that'd be 8 raycasts per floor tile, in a square "ring" around each tile?)
+// 2. if the raycast "fails" that means there's empty void there, so then instantiate a Wall tile prefab
+// 3. ... repeat until walls surround your entire floorplan
+// (technically, you will end up raycasting the same spot over and over... but the "proper" way to do this would involve keeping more lists and arrays to track all this data)
